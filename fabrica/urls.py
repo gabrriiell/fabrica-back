f540adb873aaf1b726e757c40384a70da6889f70
Gabriel gonzaga <gabrieljppb10@gmail.com>
2024-03-06 17:55:30 -0300

    .

--- fabrica/urls.py
+++ fabrica/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns=[
-    path('admin/',admin.site.url),
+    path('admin/',admin.site.urls),
     path('endereco/',include('endereco.urls'))
 ]
